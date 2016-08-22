2ea8b28ecfaa14dcc34e42f64e1e9e86dc9dd1cc
Shawn Singh <ceznik@verizon.net>
2016-08-22 15:20:17 -0400

    Update Query.js

--- app/components/Search/Query.js
+++ app/components/Search/Query.js
@@ -27,8 +27,30 @@ var Query = React.createClass({
 	render: function(){
 		return(
 			<div className="main-container">
-				<h2>Query Section</h2>
+			<div className="panel panel-default">
+				<div className="panel-heading">
+					<h3 className="panel-title text-center">Query</h3>
+				</div>
+				<div className="panel-body text-center">
+
+						<form>
+							<div className="form-group">
+								<h4 className=""><strong>Location</strong></h4>
+
+								{/*Note how each of the form elements has an id that matches the state. This is not necessary but it is convenient.
+									Also note how each has an onChange event associated with our handleChange event. 
+								*/}
+								<input type="text" className="form-control text-center" id="term" onChange= {this.handleChange} required/>
+								<br />
+								<button type="button" className="btn btn-primary" onClick={this.handleClick}>Submit</button>
+							</div>
+
+						</form>
+				</div>
 			</div>
+			</div>
+			
+			
 		)
 	}
 });
